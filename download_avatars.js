69655e469c00fcc90631e75a4d82c30ad8bb4c1e
reshux <duruhanunsal@gmail.com>
2019-03-11 17:48:33 +0000

    Step 10 is completed.

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,8 @@ var request = require('request');
 var fs = require('fs');
 var token = require('./secrets.js');
 
-var owner = process.argv[2]
-var repo = process.argv[3]
+var owner = process.argv[2];
+var repo = process.argv[3];
 
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
@@ -14,15 +14,20 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'Authorization': 'token' + token.GITHUB_TOKEN
     }
   };
-  request(options, function(err, res, body) {
-    var initObj = JSON.parse(body);
-    cb(err, initObj);
+  if (owner === undefined || repo === undefined){
+    console.log("ERROR! You have to input both a user and a repo name!")
+  } else {
+    request(options, function(err, res, body) {
+      var initObj = JSON.parse(body)
+      fs.mkdirSync('./avatars');
+      cb(err, initObj);
   });
+  }
+
 }
 
 function downloadImageByURL(url, filePath) {
   request.get(url).pipe(fs.createWriteStream(filePath));
-
 }
 
 getRepoContributors(owner, repo, function(err, result) {
