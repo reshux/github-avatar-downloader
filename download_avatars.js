c5097c3e9f7eb16a9f8df7bbd3cf2a7104b143e2
reshux <duruhanunsal@gmail.com>
2019-03-11 16:42:09 +0000

    Step 6 of the exercice is done.

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,18 @@
 var request = require('request');
+var token = require('./secrets.js')
 
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
-  request.get('https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors')
-  cb()
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token' + token.GITHUB_TOKEN
+    }
+  };
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 
