a243732db2172f1802269ca12fa55a47e2ab8fd7
reshux <duruhanunsal@gmail.com>
2019-03-11 16:23:28 +0000

    Step 3 of the exercice is done.

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,15 @@
 var request = require('request');
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+  request.get('https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors')
+  cb()
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
 console.log('Welcome to the GitHub Avatar Downloader!');
